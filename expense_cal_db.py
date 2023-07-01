078b9d8589337c564f5b573435f063a28ca3fffa
Ste881 <94097394+Ste881@users.noreply.github.com>
2023-07-01 19:56:44 +0530

    altering database for multiple users
    This is to store all expenses for different users in the same table and identify them based on the user_id

--- expense_cal_db.py
+++ expense_cal_db.py
@@ -10,32 +10,31 @@ db = mysql.connector.connect(
 
 mycursor = db.cursor()
 
-# Create a table
-def create_table():
+# Create a table to store all expenses
+def create_expenses_table():
     create_table_query = '''
         CREATE TABLE IF NOT EXISTS expenses (
             id INT AUTO_INCREMENT PRIMARY KEY,
+            user_id INT NOT NULL,
             expensetype VARCHAR(50) NOT NULL,
-            amount FLOAT NOT NULL
+            amount FLOAT NOT NULL,
+            UNIQUE KEY unique_expense (user_id, expensetype)
         )
     '''
     mycursor.execute(create_table_query)
     db.commit()
-    print('Table created')
+    print('Table expenses created')
 
-# Insert data into the table
-def insert_data():
+# Insert data into the expenses table
+def insert_expense(user_id, expensetype, amount):
     insert_data_query = '''
-        INSERT INTO expenses (expensetype, amount)
-        VALUES (%s, %s)
+        INSERT INTO expenses (user_id, expensetype, amount)
+        VALUES (%s, %s, %s)
     '''
-    data = [
-        ('Groceries', 50.25),
-        ('Utilities', 100.75)
-    ]
-    mycursor.executemany(insert_data_query, data)
+    data = (user_id, expensetype, amount)
+    mycursor.execute(insert_data_query, data)
     db.commit()
-    print('Data inserted')
+    print('Expense inserted')
 
 # Close the connection
 def close_connection():
@@ -45,6 +44,27 @@ def close_connection():
         print('Connection closed')
 
 if __name__ == '__main__':
-    create_table()
-    insert_data()
+    create_expenses_table()
+
+    user_id_1 = 1
+    user_id_2 = 2
+
+    # Sample expenses for user 1
+    expenses_user_1 = [
+        ('Food', 50.25),
+        ('Utilities', 100.75)
+    ]
+
+    # Sample expenses for user 2
+    expenses_user_2 = [
+        ('Food', 30.50),
+        ('Travel', 200.00)
+    ]
+
+    for expensetype, amount in expenses_user_1:
+        insert_expense(user_id_1, expensetype, amount)
+
+    for expensetype, amount in expenses_user_2:
+        insert_expense(user_id_2, expensetype, amount)
+
     close_connection()
